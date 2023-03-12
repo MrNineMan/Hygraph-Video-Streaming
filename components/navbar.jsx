e55c2209b8b547cfb59baf9fb297ab6255ed0399
MrNineMan <108700682+MrNineMan@users.noreply.github.com>
2023-03-12 11:28:57 +0200

    Create navbar.jsx

---
+++ components/navbar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  
+  return (
+    <nav
+      className={
+        "flex justify-between items-center p-2 px-4 border-b-4 border-zinc-500"
+      }
+    >
+      <h1 className={"text-orange-700 font-bold text-2xl"}>
+        Horoma Video Streaming
+      </h1>
+    </nav>
+  );
+}
